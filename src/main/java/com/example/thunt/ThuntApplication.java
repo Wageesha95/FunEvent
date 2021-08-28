8cd8c367ea94f68374a66a12cc8ff73b60576876
Thilina Liyanage <37930455+Wageesha95@users.noreply.github.com>
2021-08-28 18:28:31 +0530

    initial commit

---
+++ src/main/java/com/example/thunt/ThuntApplication.java
@@ -0,0 +1,13 @@
+package com.example.thunt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThuntApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThuntApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/thunt/ThuntApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thunt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThuntApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
