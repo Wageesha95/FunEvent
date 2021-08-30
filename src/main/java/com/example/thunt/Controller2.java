ba1785220a84ef1fd5ef654bc74151ff08b4483f
Thilina Liyanage <37930455+Wageesha95@users.noreply.github.com>
2021-08-31 00:53:22 +0530

    rename controllers

--- src/main/java/com/example/thunt/Controller2.java
+++
@@ -1,4 +0,0 @@
-package com.example.thunt;
-
-public class controller2 {
-}
---
+++ src/main/java/com/example/thunt/controllers/TeamTwoController.java
@@ -0,0 +1,30 @@
+package com.example.thunt.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping(value="/team2",method = RequestMethod.GET)
+public class Controller2 {
+
+    @GetMapping("/test")
+    public String test(){
+        return "test.html";
+    }
+
+    @GetMapping("/start")
+    public String stage1(){
+            return "team2/team2_stage_1.html";
+        }
+
+        @GetMapping("/2010-05-20T00:00:00.000Z")
+        public String stage2(){
+            return "team2/team2_stage_1.html";
+        }
+
+
+
+
+}
