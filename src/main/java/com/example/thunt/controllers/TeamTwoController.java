bbf060917f3c81a2afc4a70290f4201068c8c5f1
Thilina Liyanage <37930455+Wageesha95@users.noreply.github.com>
2021-08-31 01:51:13 +0530

    team 2 stage 4, team 1 tage 3

--- src/main/java/com/example/thunt/controllers/TeamTwoController.java
+++ src/main/java/com/example/thunt/controllers/TeamTwoController.java
@@ -16,13 +16,34 @@ public class TeamTwoController {
 
     @GetMapping("/start")
     public String stage1(){
+        System.out.println(" ***************************** ");
+        System.out.println(" Entered To Stage 1  :  TEAM 2  ");
+        System.out.println(" ***************************** ");
             return "team2/team2_stage_1.html";
         }
 
         @GetMapping("/2010-05-20T00:00:00.000Z")
         public String stage2(){
-            return "team2/team2_stage_1.html";
+            System.out.println(" ***************************** ");
+            System.out.println(" Entered To Stage 2  :  TEAM 2  ");
+            System.out.println(" ***************************** ");
+            return "team2/team2_stage_2.html";
         }
+
+    @GetMapping("/97km")
+    public String stage3(){
+        System.out.println(" ***************************** ");
+        System.out.println(" Entered To Stage 3  :  TEAM 2  ");
+        System.out.println(" ***************************** ");
+        return "team2/team2_stage_3.html";
+    }
+
+    @GetMapping("/Yatawara")
+    public String stage4(){
+        System.out.println(" ***************************** ");
+        System.out.println(" Entered To Stage 4  :  TEAM 2  ");
+        System.out.println(" ***************************** ");
+        return "team2/team2_stage_4.html";
 
 
 
